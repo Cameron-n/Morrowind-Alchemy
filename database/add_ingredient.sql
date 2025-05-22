c1ccfca3d47f3655137c81e45d6edc153641f0c3
Cameron-n <cameron@keithbcook.com>
2025-05-22 20:03:57 +0100

    Fix query and add as seperate sql file. Currently sql file is not used.

---
+++ database/add_ingredient.sql
@@ -0,0 +1,18 @@
+INSERT INTO Ingredient (
+	`Value`, 
+	`Weight`, 
+	`Ingredient`, 
+	`{value_property_1}`, 
+	`{value_property_2}`, 
+	`{value_property_3}`, 
+	`{value_property_4}`
+	)
+VALUES (
+	"{value_value}", 
+	"{value_weight}", 
+	"{value_ingredient}", 
+	1, 
+	1, 
+	1, 
+	1
+	);
--- pages/test.py
+++ pages/test.py
@@ -48,23 +48,23 @@ def on_add_ingredient_button_clicked(
         ):
     sql_text = f"""
     INSERT INTO Ingredient (
-        `Value`, 
-        `Weight`, 
-        `Ingredient`, 
-        `{value_property_1}`, 
-        `{value_property_2}`, 
-        `{value_property_3}`, 
-        `{value_property_4}`,
-        )
+    	`Value`, 
+    	`Weight`, 
+    	`Ingredient`, 
+    	`{value_property_1}`, 
+    	`{value_property_2}`, 
+    	`{value_property_3}`, 
+    	`{value_property_4}`
+    	)
     VALUES (
-        "{value_value}", 
-        "{value_weight}", 
-        "{value_ingredient}", 
-        1, 
-        1, 
-        1, 
-        1,
-        )
+    	"{value_value}", 
+    	"{value_weight}", 
+    	"{value_ingredient}", 
+    	1, 
+    	1, 
+    	1, 
+    	1
+    	);
     """
 
     databaseQuery(sql_text)
