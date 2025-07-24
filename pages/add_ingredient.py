03b09fb2bb3dbb41fdd33ca93a8366ffe9293601
Cameron-n <cameron@keithbcook.com>
2025-07-24 15:24:31 +0100

    Add dropdown boxes and type checks and alert for success/fail

--- pages/add_ingredient.py
+++ pages/add_ingredient.py
@@ -9,13 +9,11 @@ Page to add ingredients to the database. Password protected
 
 # TODO
 # Error handling:
-    # Property 1 empty
-    # Any cells empty
-    # Duplicate ingredient name (overwrite?)
-    # Other database errors
+    # Duplicate ingredient name (overwrite?, remove if values empty?)
 # Password protect on server
-# Feedback on success
 # Boxes to dropdown/limit input type
+    # make properties not share the same properties
+    # List of missing inputs if input is missing. Turn boxes red
 
 #%% Imports
 
@@ -23,11 +21,12 @@ Page to add ingredients to the database. Password protected
 
 # Dash
 import dash
-from dash import callback, Input, State
+from dash import callback, Input, Output, State
 import dash_mantine_components as dmc
+from sqlalchemy.exc import IntegrityError, OperationalError
 
 # Relative
-from components.data_access import db, Ingredient, DF_INGREDIENTS
+from components.data_access import db, Ingredient, DF_INGREDIENTS, DF_EFFECTS
 
 #%% Boilerplate
 
@@ -37,24 +36,24 @@ if __name__ != '__main__':
 
 #%% Layout
 
+data_origin = DF_INGREDIENTS["Origin"].unique()
+
 first_three = dmc.Group([
         dmc.NumberInput(label="Value", min=0, required=True, id="textinput_value"),
         dmc.NumberInput(label="Weight", min=0, required=True, id="textinput_weight"),
         dmc.TextInput(label="Ingredient", required=True, id="textinput_ingredient"),
-        dmc.TextInput(label="Origin", required=True, id="textinput_origin"),
+        dmc.TagsInput(label="Origin", data=data_origin, maxTags=1, required=True, id="textinput_origin"),
         ],
     grow=True,
     wrap="nowrap")
 
-# Add drop_columns to data_access
-drop_columns = ["Value", "Weight", "Ingredient", "Origin", "First Effect"]
-data = DF_INGREDIENTS.drop(drop_columns, axis=1).columns
+data_effects = DF_EFFECTS["Spell Effects"]
 
 properties = dmc.Group([
-        dmc.Select(label="Property 1", data=data, clearable=False, required=True, id="textinput_property_1"),
-        dmc.Select(label="Property 2", data=data, clearable=True, id="textinput_property_2"),
-        dmc.Select(label="Property 3", data=data, clearable=True, id="textinput_property_3"),
-        dmc.Select(label="Property 4", data=data, clearable=True, id="textinput_property_4"),
+        dmc.Select(label="Property 1", data=data_effects, clearable=False, required=True, id="textinput_property_1"),
+        dmc.Select(label="Property 2", data=data_effects, clearable=True, id="textinput_property_2"),
+        dmc.Select(label="Property 3", data=data_effects, clearable=True, id="textinput_property_3"),
+        dmc.Select(label="Property 4", data=data_effects, clearable=True, id="textinput_property_4"),
         ],
     grow=True,
     wrap="nowrap")
@@ -62,7 +61,11 @@ properties = dmc.Group([
 
 button = dmc.Button("Add Ingredient", c="myColors.9", id="button_add_ingredient")
 
+success_fail_alert = dmc.Alert(hide=True, variant='outline',
+                               withCloseButton=True, id="alert-success-fail")
+
 layout = dmc.Stack([
+    success_fail_alert,
     first_three,
     properties,
     button,
@@ -72,6 +75,11 @@ layout = dmc.Stack([
 #%% Callbacks
 
 @callback(
+    Output("alert-success-fail", "children"),
+    Output("alert-success-fail", "title"),
+    Output("alert-success-fail", "color"),
+    Output("alert-success-fail", "hide"),
+    State("alert-success-fail", "hide"),
     State("textinput_value", "value"),
     State("textinput_weight", "value"),
     State("textinput_ingredient", "value"),
@@ -81,8 +89,10 @@ layout = dmc.Stack([
     State("textinput_property_3", "value"),
     State("textinput_property_4", "value"),
     Input("button_add_ingredient","n_clicks"),
+    prevent_initial_call=True
     )
 def on_add_ingredient_button_clicked(
+        hide,
         value_value, 
         value_weight, 
         value_ingredient,
@@ -97,6 +107,11 @@ def on_add_ingredient_button_clicked(
     Adds ingredient and properties to database
     """
 
+    starred = [value_value, value_weight, value_ingredient,
+               value_origin, value_property_1]
+    if True in [i in [None, [], ""] for i in starred]:
+        return f"Missing required inputs.", "Failed!", "red", False
+
     properties = [value_property_1, value_property_2, 
                   value_property_3 , value_property_4]
 
@@ -110,6 +125,14 @@ def on_add_ingredient_button_clicked(
         First_Effect=value_property_1,
         **effects,
         )
-    
-    db.session.add(new_ingredient)
-    db.session.commit()
+
+    try:
+        # db.session.add(new_ingredient)
+        # db.session.commit()
+        1+1
+    except IntegrityError:
+        return "Duplicate ingredient name.", "Failed!", "red", False
+    except OperationalError:
+       return "Database Error. Try again later.", "Failed!", "red", False
+
+    return "Data successfully added!", "Success!", "green", False
