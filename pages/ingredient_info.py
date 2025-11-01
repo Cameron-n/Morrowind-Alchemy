af8a928a0026fc5d80b7fcfb6bba5aa6e9f2b165
Cameron-n <cameron@keithbcook.com>
2025-11-01 23:51:14 +0000

    Add draft of ingredient info page

--- pages/ingredient_info.py
+++ pages/ingredient_info.py
@@ -30,7 +30,28 @@ if __name__ != '__main__':
 
 #%% Layout
 
-layout = dmc.Text("Ingredient/Location Page [Under Construction]")
+layout = dmc.Stack([
+    dmc.Text(">>> Under Construction <<<"),
+    dmc.Select("Placeholder", w=250),
+    dmc.Image(src="Placeholder", w=100),
+    dmc.Group([
+        dmc.Group([dmc.Image(src="placeholder", w=10), "price"]),
+        dmc.Group([dmc.Image(src="placeholder", w=10), "weight"]),
+        ]),
+    dmc.Stack([
+        dmc.Title("Effects", order=4),
+        dmc.Text("effect 1"),
+        dmc.Text("effect 2"),
+        dmc.Text("effect 3"),
+        dmc.Text("effect 4"),
+        ]),
+    dmc.Stack([
+        dmc.Title("Locations", order=4),
+        dmc.Text("list"),
+        dmc.Text("of"),
+        dmc.Text("locations"),
+        ]),
+    ])
 
 
 #%% Functions
