506af07c67eb3e1de9b3087e790a907af66fd2f8
Cameron-n <cameron@keithbcook.com>
2026-03-01 18:22:04 +0000

    Convert icons to png format

---
+++ assets/icons/README.txt
@@ -0,0 +1,3 @@
+Icons in dds/tga format from base game acquired using BSA unpacker "https://www.nexusmods.com/morrowind/mods/12189"
+Icons from mods are already accessible
+Icons converted to png form using "https://www.ezyzip.com/convert-dds-to-png-online.html#"
--- assets/render_option_select.js
+++ assets/render_option_select.js
@@ -3,10 +3,11 @@ var dmc = window.dash_mantine_components;
 
 dmcfuncs.renderOptionSelect = function ({ option }) {
 
+  // Need to somehow access data from JavsScript to get image file names
   var icon = React.createElement(dmc.Image, { src: "assets/icons/MW-icon-tool-"+option.value.replace(/ /g,"_")+".png", w: 24 });
   
   if (option.value === "Journeyman's Calcinator") {
-      icon = React.createElement(dmc.Image, { src: "assets/icons/MW-icon-tool-Apprentice's_Calcinator.png", w: 24 });
+      icon = React.createElement(dmc.Image, { src: "assets/icons/tx_calcinator_01.png", w: 24 });
   }
 
   return React.createElement(
